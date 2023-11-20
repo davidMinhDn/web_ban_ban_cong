c4ae064f710878dd94150b085a01d9b0db574054
davidMinhDn <doanngocminh15112002gl@gmail.com>
2023-11-20 22:42:54 +0700

    minhdnhe
    create project spring boot

---
+++ web_ban_cong/src/main/java/com/example/web_ban_cong/WebBanCongApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_ban_cong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanCongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanCongApplication.class, args);
+	}
+
+}
---
+++ web_ban_cong/src/test/java/com/example/web_ban_cong/WebBanCongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_ban_cong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanCongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
